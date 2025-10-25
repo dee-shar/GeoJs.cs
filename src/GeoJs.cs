a717e50c146f76c522fa24a8d1a3a2ce5c46acdd
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-26 01:50:42 +0300

    Update and rename geo_js.sh to GeoJs.cs

---
+++ src/GeoJs.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Text.Json;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Security.Principal;
+using System.Security.Cryptography;
+
+namespace GeoJsApi
+{
+    public class GeoJs
+    {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://get.geojs.io/v1";
+        public GeoJs()
+        {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> GetCurrentIp()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/ip.json");
+            return await response.Content.ReadAsStringAsync();
+        }
+        public async Task<string> GetCurrentGeo()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/geo.json");
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetCurrentPtr()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/dns/ptr.json");
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetCurrentCountry()
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/ip/country.json");
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetCountryByIp(string ipAddress)
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/ip/country/{ipAddress}.json");
+            return await response.Content.ReadAsStringAsync();
+        }
+        public async Task<string> GetGeoByIp(string ipAddress)
+        {
+            var response = await httpClient.GetAsync($"{apiUrl}/ip/geo/{ipAddress}.json");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
